7d397ba8b043f8f76f2465083bd00704403f1a59
Joshua Peek <josh@joshpeek.com>
2013-11-17 10:27:02 +0000

    Add jshint gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        indent: 2,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        quotmark: true,
+        undef: true,
+        unused: true,
+        trailing: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      grunt: {
+        src: ['Gruntfile.js'],
+        options: {
+          globals: {
+            'module': false
+          }
+        }
+      },
+      src: {
+        src: ['jquery.selector-set.js'],
+        options: {
+          jquery: true
+        }
+      },
+      test: {
+        options: {
+          jquery: true,
+          globals: {
+            'test': false,
+            'ok': false,
+            'equal': false
+          }
+        },
+        src: ['test/*.js']
+      }
+    },
+    qunit: {
+      all: ['test/test.html']
+    },
+    watch: {
+      grunt: {
+        files: ['<%= jshint.grunt.src %>'],
+        tasks: ['jshint:grunt']
+      },
+      src: {
+        files: ['<%= jshint.src.src %>'],
+        tasks: ['jshint:src', 'qunit']
+      },
+      test: {
+        files: ['<%= jshint.test.src %>', 'test/*.html'],
+        tasks: ['jshint:test', 'qunit']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('test', ['jshint', 'qunit']);
+  grunt.registerTask('default', ['jshint']);
+};
